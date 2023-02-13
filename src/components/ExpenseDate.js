58ffb5ad2ae5a6f937bb7dc3ab72c01b36797868
Tony <bejjani.tony@gmail.com>
2023-02-14 09:42:31 +1100

    add expenses component as a wrapper around expenses items and apply style

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,11 +1,11 @@
 /** @format */
+
 import './ExpenseDate.css';
 
-function ExpenseDate(props) {
+const ExpenseDate = (props) => {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
-
   return (
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
@@ -13,5 +13,6 @@ function ExpenseDate(props) {
       <div className="expense-date__day">{day}</div>
     </div>
   );
-}
+};
+
 export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 /** @format */
-
-import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-
-function ExpenseItem(props) {
+import ExpenseDate from './ExpenseDate';
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
-      </div>
+      <h2 className="expense-item__description">{props.title}</h2>
+      <div className="expense-item__price">${props.amount}</div>
     </div>
   );
-}
+};
 
 export default ExpenseItem;
