1d51dad67cd904771644032788d657c5f44dc3bc
Tony <bejjani.tony@gmail.com>
2023-02-16 09:06:22 +1100

    add NewExpense form skeleton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /** @format */
 import React from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
@@ -27,6 +28,7 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,21 @@
 /** @format */
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  console.log('function called again!');
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <h2 className="expense-item__description">{props.title}</h2>
+      <h2 className="expense-item__description">{title}</h2>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
