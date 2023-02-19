064923b6af14b1b4a5dce25f288e1a8e624ec755
Tony <bejjani.tony@gmail.com>
2023-02-20 10:40:37 +1100

    apply 2 way binding to form elements upon submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -28,6 +28,10 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
 
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+
     console.log(expenseData);
   };
   return (
@@ -35,7 +39,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={enteredTitle}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -44,6 +52,7 @@ const ExpenseForm = () => {
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
+            value={enteredAmount}
           />
         </div>
         <div className="new-expense__control">
@@ -53,6 +62,7 @@ const ExpenseForm = () => {
             min="2019-01-01"
             max="2022-12-31"
             onChange={dateChangeHandler}
+            value={enteredDate}
           />
         </div>
       </div>
